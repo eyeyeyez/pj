9baac47eeb2cef04af2171e8243109b667fac347
sasithorn <sasithorneye@gmail.com>
2017-12-14 11:22:19 +0700

    kkk

---
+++ src/page/App/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+import './App.css';
+import { Image } from 'semantic-ui-react'
+
+class App extends Component {
+
+  render() {
+    return (
+      <div class="body">
+        <Image src='http://pngimagesfree.com/Heart/heart-background-ribbon-style-15.png' />
+      </div>
+    );
+  }
+}
+
+
+
+export default App;
